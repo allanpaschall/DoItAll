503a99a5bb927b489d17ec9c66f8fc3477c9a1ab
Allan Paschall <allanpaschall@gmail.cm>
2014-09-24 10:17:25 -0600

    Open Sourced it!

---
+++ CopyFiletoWebserver/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace CopyFiletoWebserver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnCopyAssurance = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(112, 99);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 0;
+            this.btnCopy.Text = "Assessment";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnCopyAssurance
+            // 
+            this.btnCopyAssurance.Location = new System.Drawing.Point(15, 99);
+            this.btnCopyAssurance.Name = "btnCopyAssurance";
+            this.btnCopyAssurance.Size = new System.Drawing.Size(75, 23);
+            this.btnCopyAssurance.TabIndex = 1;
+            this.btnCopyAssurance.Text = "Assurance";
+            this.btnCopyAssurance.UseVisualStyleBackColor = true;
+            this.btnCopyAssurance.Click += new System.EventHandler(this.btnCopyAssurance_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Copy Access files to CUB:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(47, 139);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(43, 13);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Result: ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DoItAll.Properties.Resources.doitalllogo;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(202, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(199, 161);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCopyAssurance);
+            this.Controls.Add(this.btnCopy);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Do-It-All";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnCopyAssurance;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ CopyFiletoWebserver/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Timers;
+
+namespace CopyFiletoWebserver
+{
+    public partial class Form1 : Form
+    {
+        static System.Windows.Forms.Timer _timer;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void copyAssessment()
+        {
+            try
+            {
+                File.Copy("\\\\nmmi.local\\Academic\\Assessment\\NMMIAssessmentDatabase.accdb", "\\\\cub\\inetpub\\wwwroot\\intranet\\NMMIAssessmentDatabase.accdb", true);
+                lblResult.Text = "Result: Success!";
+                resetResult();
+            }
+            catch (Exception)
+            {
+                lblResult.Text = "Result: Failed!";
+                resetResult();
+                throw;
+            }
+            
+        }
+
+        public void copyAssurance()
+        {
+            try
+            {
+                File.Copy("\\\\nmmi.local\\Academic\\Assessment\\AssuranceSystemDatabaseInterface.accdb", "\\\\cub\\inetpub\\wwwroot\\intranet\\NMMIAssuranceSystemDatabase.accdb", true);
+                lblResult.Text = "Result: Success!";
+                resetResult();
+            }
+            catch (Exception)
+            {
+                lblResult.Text = "Result: Failed!";
+                resetResult();
+                throw;
+            }
+
+        }
+
+        public void resetResult()
+        {
+            _timer = new System.Windows.Forms.Timer();
+            _timer.Tick += _timer_Tick;
+            _timer.Interval = 3000;
+            _timer.Start();
+        }
+
+        void _timer_Tick(object sender, EventArgs e)
+        {
+            lblResult.Text = "Result: ";
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            copyAssessment();
+        }
+
+        private void btnCopyAssurance_Click(object sender, EventArgs e)
+        {
+            copyAssurance();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
